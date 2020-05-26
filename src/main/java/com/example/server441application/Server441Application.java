744e71e65b7fa81db7c2b74f5eb188d7f2e0e28e
RonitTsysar <tsysar.ronit@gmail.com>
2020-05-26 11:23:06 +0300

    Initial commit

---
+++ src/main/java/com/example/server441application/Server441Application.java
@@ -0,0 +1,13 @@
+package com.example.server441application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server441Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server441Application.class, args);
+    }
+
+}
